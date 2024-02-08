2f30b839a10eda84d63d48aca95cf61cdf04217d
xdlimao <danieloliveiradesouza09@outlook.com>
2024-02-08 14:38:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const {peralta} = require('./function') //object destructuring, envia apenas a função
-
+const peraltajs = require('./function') //enviando de forma normal
 console.log(peralta())
+console.log(peraltajs.peralta())
